af0b532bfc35a4183c263a28a0101d3136fbfb46
AngelusInferna <48394752+AngelusInferna@users.noreply.github.com>
2020-10-15 06:32:51 +0200

    Update install-docker.sh
    Removed update, upgrade and reboot for faster testing

--- install-docker.sh
+++ install-docker.sh
@@ -4,7 +4,7 @@ sh get-docker.sh
 sudo apt -y install docker-compose
 sudo apt -y install unzip
 
-sudo apt-get update 
-sudo apt-get -y upgrade
+#sudo apt-get update 
+#sudo apt-get -y upgrade
 
-sudo reboot
+#sudo reboot
